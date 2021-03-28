6db3f6b98b1619ac114f0f8c02c98058742082d5
iamchapita <alejandrom646@gmail.com>
2021-03-28 17:59:18 -0600

    Implementación de main

---
+++ vpy3/main.py
@@ -0,0 +1,22 @@
+import argparse
+from tkinter import *
+from core.sudokuGame import SudokuGame
+from core.sudokuUI import SudokuUI
+
+BOARDS = ['debug', 'n00b', 'l33t', 'error']
+MARGIN = 20
+SIDE = 50
+WIDTH =  MARGIN * 2 + SIDE * 9 
+HEIGHT = MARGIN * 2 + SIDE * 9 + 100
+
+
+if __name__ == '__main__':
+
+    # with open('%s.sudoku' % board_name, 'r') as boards_file:
+    with open('n00b.sudoku', 'r') as boardFile:
+        game = SudokuGame(boardFile)
+        game.start()
+        root = Tk()
+        SudokuUI(root, game)
+        root.geometry("%dx%d" % (WIDTH, HEIGHT + 40))
+        root.mainloop()

487752c84e8fc7f16da3e8d261078e7fc87defa7
iamchapita <alejandrom646@gmail.com>
2021-03-31 17:16:40 -0600

    Cambios Menores

--- core/screenCenter.py
+++ core/screenCenter.py
@@ -6,7 +6,7 @@ class ScreenCenter:
         width = parent.winfo_reqwidth()
         height =parent.winfo_reqheight()
         print("Width", parent.winfo_screenwidth(), "Height", parent.winfo_screenheight())
-        positionRight = int(parent.winfo_screenwidth()/2 - width + 50)
+        positionRight = int(parent.winfo_screenwidth()/2 - width) #+ 50)
         positionDown = int(parent.winfo_screenheight()/2 - height)
         print(positionRight, positionDown)
         # Positions the window in the center of the page.

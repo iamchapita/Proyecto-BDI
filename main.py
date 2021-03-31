5d00c4d4ed75f6f29fa5cc4620cc0d3aa87f2d52
iamchapita <alejandrom646@gmail.com>
2021-03-31 11:49:26 -0600

    Cambios Menores

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
 import argparse
 from tkinter import *
 from core.sudokuGame import SudokuGame
-from core.sudokuUI import SudokuUI
-from core.allScreen import Screen
+from core.sudokuBoardUI import SudokuBoardUI
+from core.sudokuLoginPageUI import SudokuLoginPageUI
+from core.sudokuMainWindowUI import SudokuMainWindowUI
+from core.sudokuSplashScreenUI import SudokuSplashScreenUI
+from core.sudokuAdministratorUI import SudokuAdmnistratorUI
 
-BOARDS = ['debug', 'n00b', 'l33t', 'error']
-MARGIN = 20
-SIDE = 50
-WIDTH =  MARGIN * 2 + SIDE * 9 + 60
-HEIGHT = MARGIN * 2 + SIDE * 9 + 100
-
+boards = ['debug', 'n00b', 'l33t', 'error']
+margin = 20
+side = 50
+width =  margin * 2 + side * 9 + 60
+height = margin * 2 + side * 9 + 100
 
 if __name__ == '__main__':
 
     # with open('%s.sudoku' % board_name, 'r') as boards_file:
-    (Screen()).SplashScreen()
+    with open('core/sudoku/n00b.sudoku', 'r') as boardFile:
+        #game = SudokuGame(boardFile)
+        #game.start()
+        root = Tk()
+        #SudokuBoardUI(root, game)
+        #obj = SudokuLoginPage(root)
+        #obj = SudokuMainWindow(root)
+        obj = SudokuSplashScreenUI(root)
+        #obj = SudokuAdmnistratorUI(root)
         
-        
+        root.geometry("%dx%d" % (width, height))
+        root.mainloop()

d77ce6e709c90c89a5af0ecda3a1af8dd28b556f
iamchapita <alejandrom646@gmail.com>
2021-04-01 11:56:57 -0600

    Lineas Descomentadas

--- core/screenCenter.py
+++ core/screenCenter.py
@@ -3,8 +3,6 @@ from tkinter import *
 class ScreenCenter:
 
     def center(self, parent, width, height):
-
         positionRight = int((parent.winfo_screenwidth()/2) - (width/2))
         positionDown = int((parent.winfo_screenheight()/2) - (height/2))
-        #print(positionRight, positionDown)
         parent.geometry("+{}+{}".format(positionRight, positionDown))

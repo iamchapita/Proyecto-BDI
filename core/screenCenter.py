3da3a983374db4e5d3c7071dbcf03cd02a782883
iamchapita <alejandrom646@gmail.com>
2021-04-01 13:43:36 -0600

    Se Resto una unidad aprox. para el centrado de ventanas

--- core/screenCenter.py
+++ core/screenCenter.py
@@ -3,6 +3,8 @@ from tkinter import *
 class ScreenCenter:
 
     def center(self, parent, width, height):
-        positionRight = int((parent.winfo_screenwidth()/2) - (width/2))
-        positionDown = int((parent.winfo_screenheight()/2) - (height/2))
+        positionRight = int((parent.winfo_screenwidth() / 2) - (width / 2))
+        # Se calculó que la barra de tarea tiene 18 unidades de alto
+        # Falta probarlo en diferentes entornos gráficos
+        positionDown = int((parent.winfo_screenheight()/2) - (height/2) - 18)
         parent.geometry("+{}+{}".format(positionRight, positionDown))

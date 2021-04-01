a961cc36e9823481b86767aa2f791942bb77e34e
iamchapita <alejandrom646@gmail.com>
2021-04-01 12:11:34 -0600

    Se cambiaron parámetros para permitir la ejecución del archivo: - Se cambiaron parámetros para permitir la ejecucicón del archivo aislado de la interfaz a petición de la Doña de los gallos (Emperatríz)

--- core/sudokuAdministratorUI.py
+++ core/sudokuAdministratorUI.py
@@ -1,35 +1,40 @@
 from tkinter import *
-from core.screenCenter import ScreenCenter
+#from core.screenCenter import ScreenCenter
 
 class SudokuAdmnistratorUI(Frame):
 
-    def __init__(self, parent):
-        Frame.__init__(self, parent)
-        self.parent = parent
-        self.__initUI(parent)
+    def __init__(self):
+        self.parent = Tk()
+        super().__init__(self.parent)
+        self.pack()
+        self.__initUI()
+        self.master.mainloop()
 
-    def __initUI(self, parent):
+    def __initUI(self):
         self.margin = 70 
         self.side = 50
         self.width = self.margin * 2 + self.side * 9
         self.height = self.margin * 2 + self.side * 9 + 120 
-        self.center = ScreenCenter()
-        self.center.center(parent)
+        # self.center = ScreenCenter()
+        # self.center.center(parent)
         self.parent.title("Administración")
         self.parent.resizable(FALSE, FALSE)
         self.parent.configure(background = "white")
         self.pack(fill=BOTH)
-        self.canvas = Canvas(parent, width = self.width, height = self.height)
+        self.canvas = Canvas(self.parent, width = self.width, height = self.height)
         self.canvas.configure(background="white")
         self.canvas.pack(fill=BOTH, side=TOP)
-        createUserButton = Button(parent, text = "Crear Usuario", height = 2, width = 15, command = self. fn)
+        createUserButton = Button(self, text = "Crear Usuario", height = 2, width = 15, command = self. fn)
         createUserButton.pack()
-        editUserButton = Button(parent, text = "Editar Usuario", height = 2, width = 15, command = self. fn)
+        editUserButton = Button(self, text = "Editar Usuario", height = 2, width = 15, command = self. fn)
         editUserButton.pack()
-        deleteUserButton = Button(parent, text = "ELiminar Usuario", height = 2, width = 15, command = self. fn)
+        deleteUserButton = Button(self, text = "ELiminar Usuario", height = 2, width = 15, command = self. fn)
         deleteUserButton.pack()
-        seeLogButton = Button(parent, text = "Ver bitácora", height = 2, width = 15, command = self. fn)
+        seeLogButton = Button(self, text = "Ver bitácora", height = 2, width = 15, command = self.fn)
         seeLogButton.pack()
 
     def fn(self):
         pass
+
+
+obj = SudokuAdmnistratorUI()

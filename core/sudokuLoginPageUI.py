51f9e625ec04ee6df6e58ad8bae9f3ea25ecb2f6
iamchapita <alejandrom646@gmail.com>
2021-04-01 01:19:23 -0600

    Se cambia estructura de código: - El constructor ya no recibe un parent, se inicializa en él. - Se borró el boton de la ventana de Splash, se utiliza un temporizador para la destrucción de la misma. - Se cambió el nombre de las imágenes cargadas para usar de background e icono. - Se agrego el parámetro 'master' a la función 'PhotoImage' para indicar cual es la ventana donde debe ir la imagen (se evita un error del colector de basura) - Se estableció la dimesión del 'parent' en cada clase. - Se agrero el parámetro en la función 'pack' de canvas para asegurarse que se estire por toda el 'parent'

--- core/sudokuLoginPageUI.py
+++ core/sudokuLoginPageUI.py
@@ -1,19 +1,15 @@
 from tkinter import *
-from tkinter import ttk
-from core.sudokuBoardUI import SudokuBoardUI
+from PIL import Image
 from core.sudokuMainWindowUI import SudokuMainWindowUI
 from core.screenCenter import *
 
-class SudokuLoginPageUI(ttk.Frame):
+class SudokuLoginPageUI(Frame):
 
-    def __init__(self, parent=None):
-        if not parent:
-            parent = Tk()
-            parent.title("Inicio de Sesión")
-            parent.resizable(FALSE, FALSE)
-
-        super().__init__(parent)
-        self.pack(fill=BOTH)
+    def __init__(self):
+        
+        self.parent = Tk()
+        super().__init__(self.parent)
+        self.pack()
         self.__initUI()
         self.master.mainloop()
 
@@ -22,18 +18,25 @@ class SudokuLoginPageUI(ttk.Frame):
         # Investigar como usar un GRID para ubicar objetos
         # en las ventanas
         # Se usa coordenadas para ubicar objetos en la ventana
-        
-        self.logoImage = PhotoImage(file = "core/images/SudokuLogo.png")
-        canvasObject = Canvas(self,height=700, width=400)
-        l_logo = Label(self,image=self.logoImage)
-        l_logo.place(x=-45, y=150, relwidth=1, relheight=1)
-        canvasObject.pack()
-
-        ttk.Label(self, text="Nombre de Usuario",font=("Calibri","20")).place(x=80, y=150)
-        ttk.Entry(self).place(x=120, y=190)
-        ttk.Label(self, text="Contraseña",font=("Calibri","20")).place(x=120, y=250)
-        ttk.Entry(self).place(x=120, y=290)
-        ttk.Button(self, text="Iniciar Sesión", command = self.__onClick).place(x=155, y=330)
+        self.width = 400
+        self.height = 600
+        self.logo = PhotoImage(file="core/images/SudokuLogo.png", master=self.parent)
+        self.parent.title("Inicio de Sesión")
+        self.parent.resizable(False, False)
+        self.parent.geometry("%dx%d"%(self.width, self.height))
+        self.parent.iconphoto(True, self.logo)
+        center = ScreenCenter()
+        center.center(self.parent, self.width, self.height)
+        canvas = Canvas(self, width=self.width, height=self.height)
+        canvas.pack(fill=BOTH, expand = True)
+        canvas.create_image(200, 300, image=self.logo)
+        # NO HAY TRANSPARENCIAS PARA EL BACKGROUND DEL LABEL :) ODIO Tkinter, Amo pyQt5
+        canvas.create_window(200, 50, window=Label(self,text="Inicio de Sesión",font=("Calibri","20")))
+        canvas.create_window(200, 150, window=Label(self, text="Nombre de Usuario",font=("Calibri","20")))
+        canvas.create_window(200, 190, window=Entry(self))
+        canvas.create_window(200, 250, window=Label(self, text="Contraseña", font=("Calibri", "20")))
+        canvas.create_window(200, 290, window=Entry(self).place(x=120, y=290))
+        canvas.create_window(200, 430, window=Button(self, text="Iniciar Sesión", command = self.__onClick))
 
     def __onClick(self):
         self.destroy()
--- core/sudokuSplashScreenUI.py
+++ core/sudokuSplashScreenUI.py
@@ -1,36 +1,34 @@
 from tkinter import *
-from tkinter import ttk
 from core.screenCenter import ScreenCenter
 from core.sudokuLoginPageUI import SudokuLoginPageUI
 
-class SudokuSplashScreenUI(ttk.Frame):
+class SudokuSplashScreenUI(Frame):
 
-    def __init__(self, parent=None):
-        if not parent:
-            parent = Tk()
-            parent.title("Bienvenido")
-            parent.resizable(FALSE, FALSE)
-            self.logo = PhotoImage(file = "core/images/SudokuLogo.png")
-            parent.iconphoto(True,self.logo)
-
-
-        self.logoImage = PhotoImage(file = "core/images/WelcomeScreen.png")
-        super().__init__(parent)
+    def __init__(self):
+        
+        self.parent = Tk()
+        super().__init__(self.parent)
         self.pack()
         self.__initUI()
         self.master.mainloop()
 
     def __initUI(self):
-        
-        canvasObject = Canvas(self,height=self.logoImage.height(), width=self.logoImage.width())
-        l_logo = Label(self,image=self.logoImage)
-        l_logo.place(x=0, y=0, relwidth=1, relheight=1)
-        canvasObject.grid(row=0,column=0)
-
-        ttk.Button(self, text = "Entrar", command = self.goToLoginPage).grid(pady=40)
+        self.icon = PhotoImage(file="core/images/SudokuLogo.png", master=self.parent)
+        self.backgroundImage = PhotoImage(file="core/images/WelcomeScreen.png", master=self.parent)
+        self.parent.title("Bienvenido")
+        self.parent.resizable(False, False)
+        self.parent.iconphoto(True, self.icon)
+        self.parent.geometry("%dx%d" % (self.backgroundImage.width(), self.backgroundImage.height()))
+        center = ScreenCenter()
+        center.center(self.parent, self.backgroundImage.width(), self.backgroundImage.height())
+        canvas = Canvas(self, width=self.backgroundImage.width(), height=self.backgroundImage.height())
+        labelLogo = Label(self,image=self.backgroundImage)
+        labelLogo.place(x=0, y=0, relwidth=1, relheight=1)
+        canvas.grid(row=0, column=0)
+        self.after(1000, self.goToLoginPage)
 
     def goToLoginPage(self):
-        self.destroy()
-        SudokuLoginPageUI(self.master)
+        self.parent.destroy()
+        SudokuLoginPageUI()
         
     

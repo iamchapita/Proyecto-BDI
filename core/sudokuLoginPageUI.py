143c4cc16c7b0f3f796555634c8ef56f12581e00
itsdddaniel <xdarktheboss@gmail.com>
2021-04-01 15:00:24 -0600

    Se modifico visualmente

--- core/sudokuLoginPageUI.py
+++ core/sudokuLoginPageUI.py
@@ -14,9 +14,6 @@ class SudokuLoginPageUI(Frame):
 
     def __initUI(self):
 
-        # Investigar como usar un GRID para ubicar objetos
-        # en las ventanas
-        # Se usa coordenadas para ubicar objetos en la ventana
         self.width = 400
         self.height = 600
         self.logo = PhotoImage(file="core/images/SudokuLogo.png", master=self.parent)
@@ -31,15 +28,11 @@ class SudokuLoginPageUI(Frame):
         labelLogo = Label(self,image=self.backgroundImage)
         labelLogo.place(x=0, y=0, relwidth=1, relheight=1)
         canvas.grid(row=0, column=0)
-        #canvas.create_image(200, 465, image=self.logo)
         # NO HAY TRANSPARENCIAS PARA EL BACKGROUND DEL LABEL :) ODIO Tkinter, Amo pyQt5
         # viva pyQt5!
-        #canvas.create_window(200, 50, window=Label(self,text="Inicio de Sesión",font=("Calibri","20")))
-        #canvas.create_window(200, 150, window=Label(self, text="Nombre de Usuario",font=("Calibri","20")))
-        canvas.create_window(200, 190, window=Entry(self))
-        #canvas.create_window(200, 250, window=Label(self, text="Contraseña", font=("Calibri", "20")))
-        canvas.create_window(200, 300, window=Entry(self))
-        canvas.create_window(200, 335, window=Button(self, text="Iniciar Sesión", command = self.__onClick))
+        canvas.create_window(200, 120, window=Entry(self))
+        canvas.create_window(200, 250, window=Entry(self))
+        canvas.create_window(200, 300, window=Button(self, text="Iniciar Sesión", width=15, height=2, command = self.__onClick))
 
     def __onClick(self):
         self.parent.destroy()

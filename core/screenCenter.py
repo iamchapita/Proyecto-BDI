5a515a525fbf4d64cb041fbba7378ec0ef5721ff
iamchapita <alejandrom646@gmail.com>
2021-04-01 23:14:50 -0600

    Se editarón los parámetros de la función geometry. Se asignó solamente la tarea de centrar la ventana al método center.

--- core/screenCenter.py
+++ core/screenCenter.py
@@ -5,4 +5,4 @@ class ScreenCenter:
     def center(self, parent, width, height):
         xCoordinate = int(parent.winfo_screenwidth()/2 - width/2)
         yCoordinate = int(parent.winfo_screenheight()/2 - height/2)
-        parent.geometry('%dx%d+%d+%d' % (width, height, xCoordinate, yCoordinate))
+        parent.geometry('+%d+%d' % (xCoordinate, yCoordinate))

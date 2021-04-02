cc6e0c170f94d3e68fd672fc1182069b5ee79f0e
iamchapita <alejandrom646@gmail.com>
2021-04-02 00:24:46 -0600

    Cambios Varios: - Se agregaron comentarios - Se agrego el icono a la ventana - Se comentó el color de background del parent

--- core/sudokuAdministratorUI.py
+++ core/sudokuAdministratorUI.py
@@ -15,22 +15,20 @@ class SudokuAdmnistratorUI(Frame):
         self.master.mainloop()
 
     def __initUI(self):
+        # Se debe utilizar la ruta core/images/SudokuLogo.png al implementarlo en el main
+        self.icon = PhotoImage(file="images/SudokuLogo.png", master=self.parent)
         self.width = 400
         self.height = 600
         self.parent.title('Opciones Administrador')
-
-        #Tamaño de la ventana
-        self.parent.geometry("400x600")
-        self.parent.configure(background = "#413c3d")
-
-        #Mantiene la ventana fija para evitar que el diseño se vea afectado
+        self.parent.iconphoto(True, self.icon)
+        # Tamaño de la ventana
+        self.parent.geometry("%dx%d" % (self.width, self.height))
+        # Este color esta dispuesto a cambio
+        # self.parent.configure(background = "#413c3d")
         self.parent.resizable(False, False)
-        
         TitleStyles = tkFont.Font(family="Lucida Grande", size=18)
-        
         self.center = ScreenCenter()
         self.center.center(self.parent, self.width, self.height)
-
         Button(self.parent, text = 'Crear usuario', bg="#6ea8d9", font=TitleStyles, command= self.goCreateUser).place(x=100, y=110, height = 50, width = 210)
         Button(self.parent, text = 'Editar usuario', bg="#6ea8d9", font=TitleStyles, command= self.goEditUser).place(x=100, y=190, height = 50, width = 210)
         Button(self.parent, text = 'Eliminar usuario', bg="#6ea8d9", font=TitleStyles, command= self.goDeleteUser).place(x=100, y=270, height = 50, width = 210)

52d7165a3bcef508160a26b4ba578629e493aa3a
iamchapita <alejandrom646@gmail.com>
2021-04-03 01:43:18 -0600

    Añadida función SQL de comparación de parámetros.

---
+++ Scripts de Base de Datos/Functions.sql
@@ -0,0 +1,20 @@
+USE SudokuDB;
+
+SET GLOBAL log_bin_trust_function_creators = 1;
+
+DELIMITER $$
+    DROP FUNCTION IF EXISTS fn_compareData;
+    CREATE FUNCTION fn_compareData(tex_pynickname TEXT, tex_pypassword TEXT) RETURNS TINYINT
+    BEGIN
+
+        SET @password = (SELECT tex_password FROM User WHERE tex_nickname = tex_pynickname);
+        SET @result = IF(@password = AES_ENCRYPT(HEX((tex_pypassword)), @username), 1, 0);
+
+        RETURN @result;
+
+    END $$
+
+DELIMITER ;
+
+
+SELECT fn_compareData("admin", "admin") AS Data

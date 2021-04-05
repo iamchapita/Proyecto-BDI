5b1e640be2dd3bf23afbb4b9625c65ce02b0c08a
iamchapita <alejandrom646@gmail.com>
2021-04-04 18:21:10 -0600

    Cambios Varios: - Se cambio estructura de inicialización de ventana - Se cambio el texto del botón a solo "Si" - Se agrego un botón con el texto "No" - Se renombró la funnción __salir a __getOut - Se creo la función __cancel que es llamada por el botón 2 ("No") - Cambios mínimos en la función __getOut

--- Codigo/core/DialogClose.py
+++ Codigo/core/DialogClose.py
@@ -9,28 +9,27 @@ opciones de  de salida de la aplicación y minimizar pantalla.
 @author Daniel Arteaga, Kenneth Cruz, Gabriela Hernández, Luis Morales
 @version 1.0
 """
-class DialogClose:
+class DialogClose(Toplevel):
 
-    """
-    Constructor de la clase.
-    @author Daniel Arteaga, Kenneth Cruz, Gabriela Hernández, Luis Morales
-    @version 1.0
-    """
     def __init__(self, parent):
-        self.top = Toplevel(parent)
+        Toplevel.__init__(self)
         self.parent = parent
-        self.top.configure(background = "#171717")
-        self.top.title("Salir")
+        # Obliga al parent a permanecer congelado hasta que la ventana DialogClose se cierre
+        self.grab_set()
+        self.configure(background = "#171717")
+        self.title("Salir")
 
         self.center= ScreenCenter()
-        self.center.center(self.top, 150, 490)
+        self.center.center(self, 150, 490)
 
-        label1 = Label(self.top, text="¿Está seguro?",font =("Lato",15))
+        label1 = Label(self, text="¿Está seguro?",font =("Lato",15))
         label1.configure(background = "#171717", fg="white")
         label1.grid(row=0, column=0, columnspan=2)
 
-        self.button1 = Button(self.top, text="Si, salir del juego.", bg="#6ea8d9", font=("Lato",12), command=self.__salir)
+        self.button1 = Button(self, text="Si", bg="#6ea8d9", font=("Lato",12), command=self.__getOut)
         self.button1.grid(row=1, column=0, padx=5, pady=5)
+        self.button2 = Button(self, text="No", bg="#6ea8d9", font=("Lato",12), command=self.__cancel)
+        self.button2.grid(row=1, column=2, padx=5, pady=5)
 
     """
     Función que muestra una ventana y preguntar si el usuario se quiere
@@ -38,8 +37,11 @@ class DialogClose:
     @author Daniel Arteaga, Kenneth Cruz, Gabriela Hernández, Luis Morales
     @version 1.0
     """
-    def __salir(self):
-        self.top.destroy()
+    def __getOut(self):
+        self.destroy()
         self.parent.destroy()
         SudokuBye()
         sys.exit()
+    
+    def __cancel(self):
+        self.destroy()

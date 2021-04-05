624f91f0169d7e3fc9f3eab5808e5344e40e2239
iamchapita <alejandrom646@gmail.com>
2021-04-05 10:58:34 -0600

    Corregido error de paentesis en @result

--- Scripts de Base de Datos/Functions.sql
+++ Scripts de Base de Datos/Functions.sql
@@ -18,7 +18,8 @@ DELIMITER $$
     );
     SET @passwordResult = IF(@password = HEX(AES_ENCRYPT(pyPassword, pyNickname)), 1, 0);
     SET @rolResult = IF((SELECT bit_rol FROM User WHERE tex_nickname = pyNickname) = 1, 1, 0);
-    SET @result = (SELECT CONCAT(@nicknameResult, " ", @passwordResult, " " ,@rolResult));
+    SET @newPasswordResult = IF((SELECT HEX(AES_ENCRYPT(pyPassword, pyNickname)) FROM User WHERE tex_nickname = pyNickname ) = @password, 1, 0);
+    SET @result = (SELECT CONCAT(@nicknameResult, " ", @passwordResult, " " ,@rolResult, " ", @newPasswordResult));
     RETURN @result;
 
     END $$

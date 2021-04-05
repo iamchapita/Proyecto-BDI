3f7fe48125f0b6873c8c0148010187304056db32
iamchapita <alejandrom646@gmail.com>
2021-04-04 19:59:21 -0600

    Cambios varios - Se configuró un grid - Se estableció posición de elementos de acuerdo al grid - Se le estableció un acho por defecto los botones "Si" y "No"

--- Codigo/core/DialogClose.py
+++ Codigo/core/DialogClose.py
@@ -13,23 +13,43 @@ class DialogClose(Toplevel):
 
     def __init__(self, parent):
         Toplevel.__init__(self)
+        # Se configuran 3 columnas para el grid
+        self.columnconfigure(0, weight = 1)
+        self.columnconfigure(1, weight = 1)
+        self.columnconfigure(2, weight=1)
+        # Se configuran 3 filas para el grid
+        self.rowconfigure (0, weight = 1 )
+        self.rowconfigure (1, weight = 1 )
+        self.rowconfigure (2, weight = 1 )
         self.parent = parent
         # Obliga al parent a permanecer congelado hasta que la ventana DialogClose se cierre
         self.grab_set()
         self.configure(background = "#171717")
         self.title("Salir")
 
+        self.width = 400
+        self.height = 150
+        self.geometry("{}x{}".format(self.width, self.height))
+
         self.center= ScreenCenter()
-        self.center.center(self, 150, 490)
-
+        self.center.center(parent=self, width=self.width, height=self.height)
+        
+        """ 
+        UTILICE ESTOS CICLOS PARA ENTENDER EL GRID 
+        for r in range(3):
+            for c in range(3):
+                label1 = Label(self, text="¿Está seguro?",font =("Lato",15))
+                label1.configure(background="#171717", fg="white")
+                label1.grid(row=r, column=c) 
+        """
+        
         label1 = Label(self, text="¿Está seguro?",font =("Lato",15))
-        label1.configure(background = "#171717", fg="white")
-        label1.grid(row=0, column=0, columnspan=2)
-
-        self.button1 = Button(self, text="Si", bg="#6ea8d9", font=("Lato",12), command=self.__getOut)
-        self.button1.grid(row=1, column=0, padx=5, pady=5)
-        self.button2 = Button(self, text="No", bg="#6ea8d9", font=("Lato",12), command=self.__cancel)
-        self.button2.grid(row=1, column=2, padx=5, pady=5)
+        label1.configure(background="#171717", fg="white")
+        label1.grid(row=1, column=1)
+        self.button1 = Button(self, text="Si", bg="#6ea8d9", font=("Lato",12), width = 10, command=self.__getOut)
+        self.button1.grid(row=2, column=0, padx=5, pady=5)
+        self.button2 = Button(self, text="No", bg="#6ea8d9", font=("Lato",12), width = 10, command=self.__cancel)
+        self.button2.grid(row=2, column=2, padx=5, pady=5)
 
     """
     Función que muestra una ventana y preguntar si el usuario se quiere

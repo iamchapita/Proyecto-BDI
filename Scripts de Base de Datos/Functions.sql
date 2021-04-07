f483a9a03070e1fd6dbdcd561a440d8ee103a82e
lowenmensch-tz <kenneth-cruz@outlook.com>
2021-04-07 00:08:23 -0600

    Se agrega condición que toma en cuenta a los usuarios que estan de baja

--- Scripts de Base de Datos/Functions.sql
+++ Scripts de Base de Datos/Functions.sql
@@ -14,7 +14,8 @@ DELIMITER $$
         FROM
             User
         WHERE
-            tex_nickname = pyNickname
+            tex_nickname = pyNickname AND 
+            bit_state = 1
     );
     SET @passwordResult = IF(@password = HEX(AES_ENCRYPT(pyPassword, pyNickname)), 1, 0);
     SET @rolResult = IF((SELECT bit_rol FROM User WHERE tex_nickname = pyNickname) = 1, 1, 0);

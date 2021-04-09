7ddedb672ae699ba53bfd0bd88177bad2725e2e0
lowenmensch-tz <kenneth-cruz@outlook.com>
2021-04-09 09:34:21 -0600

    Se agrega una función que inserta los datos de los tablero y actualiza los estados de esos tableros

--- Codigo/core/EngineSQL/MySQLToolConnection.py
+++ Codigo/core/EngineSQL/MySQLToolConnection.py
@@ -35,3 +35,54 @@ class ToolConnection:
 
         #(id, name)
         return (transaction[0][0], transaction[0][1])
+
+
+    """
+        Creación de una partida dentro del Board
+        @param: idUsername, idBoard, cod_state, time, stack
+    """
+    def insertGameBoard(self, idUsername, idBoard, state=1, time="00:00:00", stack=[]):
+        
+        self.db.insert(
+                table="Game", 
+                fields=[
+                            "id_user_fk",
+                            "id_sudokuboard_fk", 
+                            "blo_file", 
+                            "tim_time"
+                        ], 
+                values=[
+                            idUsername, 
+                            idBoard, 
+                            "{}".format( stack ), 
+                            time
+                ]
+            )
+            
+        query = """
+                    SELECT 
+                        id
+                    FROM 
+                        Game 
+                    WHERE 
+                        id_user_fk={} AND tim_time='00:00:00'
+                    ORDER BY 
+                        tim_date DESC
+                    LIMIT 1
+                    ;
+                """.format( idUsername )
+
+        #Estado de la partida:  1 nuevo, 2 pausado, 3 finalizado, 4 derrota 
+        self.db.insert(
+                table="State", 
+                fields=[
+                            "id_game_fk",
+                            "cod_state"
+                        ], 
+                values=[
+                            self.db.select(query=query)[0][0], 
+                            state
+                        ]
+            )
+        
+        self.db.closeConnection()

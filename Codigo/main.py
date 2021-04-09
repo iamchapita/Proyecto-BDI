fc31e2ca2217affedb998587d2ef1bed60ebe954
iamchapita <alejandrom646@gmail.com>
2021-04-09 11:34:42 -0600

    Cambios menores

--- Codigo/main.py
+++ Codigo/main.py
@@ -2,13 +2,6 @@ import argparse
 from tkinter import *
 from core.SudokuSplashScreenUI import SudokuSplashScreenUI
 
-boards = ['debug', 'n00b', 'l33t', 'error']
-margin = 20
-side = 50
-width =  margin * 2 + side * 9 + 60
-height = margin * 2 + side * 9 + 100
-
 if __name__ == '__main__':
     
-    with open('core/sudoku/n00b.sudoku', 'r') as boardFile:
-        obj = SudokuSplashScreenUI()
+    obj = SudokuSplashScreenUI()

7790d3741d37feca4dcd0355128316f6c5a174eb
lowenmensch-tz <kenneth-cruz@outlook.com>
2021-04-10 00:28:58 -0600

    No hay nada aquí, pero no me deja hacer git pull

--- Codigo/core/SudokuBoardUI.py
+++ Codigo/core/SudokuBoardUI.py
@@ -1,6 +1,6 @@
 from tkinter import *
 from core.ScreenCenter import ScreenCenter
-from core.DialogClose import DialogClose
+#from core.DialogClose import DialogClose
 from core.EngineSQL.MySQLEngine import MySQLEngine
 from core.EngineSQL.ConfigConnection import ConfigConnection
 from core.EngineSQL.MySQLToolConnection import ToolConnection
